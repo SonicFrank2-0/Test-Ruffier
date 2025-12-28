4c11e3655337546b210e6029d9acf648c72d4fc8
SonicFrank2-0 <frankra190211@gmail.com>
2025-12-28 14:59:11 -0600

    Version 1.1

---
+++ instr.py
@@ -0,0 +1,11 @@
+from PyQt5.QtCore import QTime
+#Variables para las coordenadas en donde va a aparecer la ventana de la app
+win_x = 200
+win_y = 100
+#Variables para el tamaño de la ventana
+win_width = 1000
+win_height = 600
+#Variables de la primera pantalla
+txt_hello = "Bienvenido al programa de diagnostico de salud"
+txt_next = "Inicio"
+txt_title = "Test Ruffier" 
